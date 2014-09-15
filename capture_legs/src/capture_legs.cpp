99dd70a361b620fc076a8fa1f81f641752efde8f
Jake Reher <jake.reher@gmail.com>
2014-09-15 16:59:05 -0500

    Changed so ekf runs at 200hz

--- capture_legs/src/capture_legs.cpp
+++ capture_legs/src/capture_legs.cpp
@@ -11,15 +11,16 @@
 //---------------------------------------------------------------------------------------------------
 
 #include "ambcap.h"
+#include "ambcap_EKF.h"
 
 main(int argc, char** argv)
 {
-	int frequency = 100; // Default value of frequency is 100hz
+    int frequency = 200; // Default value of frequency is 100hz
     ros::init(argc, argv, "capture_legs");
     ros::NodeHandle nh;
     ros::param::get("/frequency", frequency);
 
-    ambcap AC(nh,frequency);
+    ambcap_EKF AC(nh,frequency);
     AC.spin();
 
     return 0;

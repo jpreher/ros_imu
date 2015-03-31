9b4820ec8e1e6fe286e60762994350d2105cb37a
jreher <jreher@jreher-aames-laptop.(none)>
2015-03-31 13:30:33 -0500

    Scripts now in project path

--- yei_sensor/setup.py
+++ yei_sensor/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     version='0.0.0',
-    scripts=['bin/yei_sensor'],
+    scripts=['bin/yei_sensor.py'],
     packages=['yei_sensor'],
     package_dir={'': 'src'}
 )
